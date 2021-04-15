7566cc0f19ffd613a0552f4697370d38e9f77047
radiscipio <robertdiscipio@gmail.com>
2021-04-15 17:51:20 -0400

    Put input into state

--- src/App.js
+++ src/App.js
@@ -2,30 +2,20 @@ import { Component } from "react";
 import "./App.css";
 import Buttons from "./components/buttons";
 
-class App extends Component {
-
+export default class App extends Component {
   constructor(props) {
+    super(props);
 
-    super(props);  
-    this.state =  {
-
+    this.state = {
+      input: "",
+    };
   }
-  this.render(){
+
+  render() {
     return (
       <div className="App">
-        <input
-          className="CalculatorInput"
-          value={input.input}
-          type="text"
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          placeholder="0"
-        />
-  
         <Buttons />
       </div>
     );
   }
-
-  
 }
