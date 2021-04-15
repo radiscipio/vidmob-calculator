20d84d77f6fbd58b9a1783d6fcfaaf8b5189cc61
radiscipio <robertdiscipio@gmail.com>
2021-04-15 17:42:01 -0400

    Updated file structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
-import Buttons from './components/buttons';
+import { Component } from "react";
+import "./App.css";
+import Buttons from "./components/buttons";
 
-function App() {
-  return (
-    <div className="App">
-      <Buttons />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+
+    super(props);  
+    this.state =  {
+
+  }
+  this.render(){
+    return (
+      <div className="App">
+        <input
+          className="CalculatorInput"
+          value={input.input}
+          type="text"
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          placeholder="0"
+        />
+  
+        <Buttons />
+      </div>
+    );
+  }
+
+  
 }
-
-export default App;
