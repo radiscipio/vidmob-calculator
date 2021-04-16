95330c9b2d073e3a89fa48cdd5f7e3cb13159da1
radiscipio <robertdiscipio@gmail.com>
2021-04-15 20:35:07 -0400

    Updated css for calc

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -7,34 +7,34 @@ export default class Buttons extends Component {
 
       <div className="buttons-container">
         <div className="buttons-row">
-          <div className="buttons">←</div>
           <div className="buttons">(</div>
           <div className="buttons">)</div>
+          <div className="buttons">←</div>
           <div className="buttons">AC</div>
         </div>
         <div className="buttons-row">
           <div className="buttons">7</div>
           <div className="buttons">8</div>
           <div className="buttons">9</div>
-          <div className="buttons">*</div>
+          <div className="buttons operator">*</div>
         </div>
         <div className="buttons-row">
           <div className="buttons">4</div>
           <div className="buttons">5</div>
           <div className="buttons">6</div>
-          <div className="buttons">÷</div>
+          <div className="buttons operator">÷</div>
         </div>
         <div className="buttons-row">
           <div className="buttons">1</div>
           <div className="buttons">2</div>
           <div className="buttons">3</div>
-          <div className="buttons">-</div>
+          <div className="buttons operator">-</div>
         </div>
         <div className="buttons-row">
           <div className="buttons">.</div>
           <div className="buttons">0</div>
-          <div className="buttons">=</div>
-          <div className="buttons">+</div>
+          <div className="buttons operator">=</div>
+          <div className="buttons operator" >+</div>
         </div>
       </div>
     );
