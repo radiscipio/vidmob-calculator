ca7b28d63780307db795518aaead8c2ba403f614
radiscipio <robertdiscipio@gmail.com>
2021-04-18 22:31:20 -0400

    Handles parens, but only from the beginning

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,37 +1,26 @@
 let parensCheck = (input) => {
   
-}
+let parens = ["(", ")"]
+let parensArr = []
 
-let splitOperators = (input) => {
-  let startIndex = 0;
-  let newInput = [];
-
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] === "-") {
-      newInput.push(input.substring(startIndex, i));
-      newInput.push("-");
-      startIndex = i + 1;
-    } else if (input[i] === "+") {
-      newInput.push(input.substring(startIndex, i));
-      newInput.push("+");
-      startIndex = i + 1;
-    } else if (input[i] === "*" || input[i] === "x") { 
-
-      newInput.push(input.substring(startIndex, i));
-      newInput.push("*");
-      startIndex = i + 1;
-    } else if (input[i] === "/" || input[i] === "÷") {
-      newInput.push(input.substring(startIndex, i));
-      newInput.push("/");
-      startIndex = i + 1;
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === parens[0] && input[i + 1] === parens[1]) {
+    input.splice(i, 0, '*')
+    input.push(parensArr)
     }
   }
-  newInput.push(input.substring(startIndex, input.length));
-  return newInput;
-};
+  return input
+}
 
-let doTheMath = (str) => {
-
+// let parensthesis = (input) => {
+//   let parens = ("(", ")")
+//   for (let i = 0; i < input.length; i++) {
+//     if(input.includes("(") && input.includes(")")) {
+//        input.
+//     }
+//   }
+// }
+let pemdas = (str) => {
   const input = splitOperators(str);
   for (let i = 0; i < input.length; i++) {
     if (input[i] === "*") {
@@ -56,13 +45,54 @@ let doTheMath = (str) => {
     }
   }
   return input[0].toString();
+}
+
+
+let splitOperators = (input) => {
+  let startIndex = 0;
+  let newInput = [];
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "-") {
+      newInput.push(input.substring(startIndex, i));
+      newInput.push("-");
+      startIndex = i + 1;
+    } else if (input[i] === "+") {
+      newInput.push(input.substring(startIndex, i));
+      newInput.push("+");
+      startIndex = i + 1;
+    } else if (input[i] === "*" || input[i] === "x") { 
+      newInput.push(input.substring(startIndex, i));
+      newInput.push("*");
+      startIndex = i + 1;
+    } else if (input[i] === "/" || input[i] === "÷") {
+      newInput.push(input.substring(startIndex, i));
+      newInput.push("/");
+      startIndex = i + 1;
+    }
+  }
+  newInput.push(input.substring(startIndex, input.length));
+  return newInput;
 };
+
+let doTheMath = (str) => {
+  let result = ""
+  if (str.includes("(") && str.includes(")")) {
+    let parensOpen = str.indexOf("(")
+    let parensClose = str.indexOf(")")
+    let parensValues = str.substring(parensOpen, parensClose + 1)
+    let parensFunction = pemdas(str.substring(parensOpen + 1, parensClose + 1))
+    result = str.replace(parensValues, parensFunction)
+  }
+  return result
+};
+
 
 // console.log(splitOperators("10+9-4*4"));
 // console.log(doTheMath("4+4"));
-// console.log(doTheMath("4*4"));
+console.log(doTheMath("4(4+5)"));
 // console.log(doTheMath("12/4"));
-console.log(doTheMath("10+9-4*4"));
+// console.log(doTheMath("10+9-4*4"));
 
 
-export default doTheMath
+// export default doTheMath
